1f141d3a4a271c66833228fdb01ac31d4be9a8fd
Damien 'disnaut' Rodriguez <damienr2017@gmail.com>
2022-05-03 02:29:31 -0400

    Initial Commit
    Tests are stubbed out, Database Init file has been created, notes.txt has been added.

---
+++ unit tests/Test_DatabaseInit.py
@@ -0,0 +1,25 @@
+import pytest
+
+def test_HashFile_Does_Not_Exist():
+    assert True == True
+
+def test_HashFile_Download():
+    assert True == True
+
+def test_HashFile_Exists():
+    assert True == True
+
+def test_Database_Does_Not_Exist():
+    assert True == True
+
+def test_Database_Download():
+    assert True == True
+
+def test_Database_Exists():
+    assert True == True
+
+def test_HashFile_Compared_To_Database_Hash():
+    assert True == True
+
+def test_HashFile_Compared_To_Other_File():
+    assert True == True
