6bfeadc505e31f7faf6540f0db56fa4f89a2cfb3
Damien 'disnaut' Rodriguez <damienr2017@gmail.com>
2022-05-03 17:52:19 -0400

    Working Database Validation and Download
    This commit is several other commits that were dropped due to my incompetence. The issue has since been resolved.

--- modules/DatabaseInit.py
+++ modules/DatabaseInit.py
@@ -1,12 +1,57 @@
-def main():
-    print("Hello, World!")
-main()
+from os.path import exists, dirname
+import os
+import requests
+import hashlib
 
 def FileExists(strFileName):
-    return True
+    return exists(os.getcwd() + '/' + strFileName)
 
-def DownloadFile(strDownloadUrl):
-    return True
+def DownloadFile(strDownloadUrl, strFileName):
+    try:
+        fileIn = requests.get(strDownloadUrl)
+        fileOut = open(os.getcwd() + '/' + strFileName,'wb').write(fileIn.content)
+        fileIn.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 def HashCompare(strFileName):
-    return True
+    strDownloadedDatbaseHash = LoadHashFile()
+    strCalculatedHash = CalculateHash(strFileName)
+    return strDownloadedDatbaseHash == strCalculatedHash
+
+def LoadHashFile():
+    fileIn = open('MTG_Database.sqlite.sha256', 'r')
+    strHash = fileIn.readline().strip()
+    fileIn.close()
+    return strHash
+
+def CalculateHash(strFileName):
+    strHash = ''
+    with open(strFileName,'rb') as f:
+        bytes = f.read()
+        strHash = hashlib.sha256(bytes).hexdigest()
+    f.close()
+    return strHash
+
+def main():
+    blnNewDatabase = False
+    #Download the hash file, keep the name
+    DownloadFile('https://mtgjson.com/api/v5/AllPrintings.sqlite.sha256', 'AllPrintings.sqlite.sha256')
+    #Check to see if a hash file already exists
+    if FileExists('MTG_Database.sqlite.sha256') == False:
+    #If it does not exist, make the downloaded hash file the current hash file
+        os.rename('AllPrintings.sqlite.sha256', 'MTG_Database.sqlite.sha256')
+    else:
+    #If it exists, compare the hashes
+        if HashCompare('AllPrintings.sqlite.sha256') == False:
+            os.remove('MTG_Database.sqlite.sha256')
+            os.rename('AllPrintings.sqlite.sha256', 'MTG_Database.sqlite.sha256')
+            blnNewDatabase = True
+
+    #If the file does not exist, or
+    if FileExists('MTG_Database.sqlite') == False or blnNewDatabase:
+        DownloadFile('https://mtgjson.com/api/v5/AllPrintings.sqlite', 'MTG_Database.sqlite')
+
+main()
--- unit tests/Test_DatabaseInit.py
+++ unit tests/Test_DatabaseInit.py
@@ -1,6 +1,7 @@
 import pytest
 import sys
 import os
+import time
 #This is to make sure that the modules are imported correctly.
 #This directly modifies your python path. This may have to be
 #changed because of security.
@@ -8,28 +9,45 @@ os.chdir(os.path.dirname(os.getcwd()))
 sys.path.insert(1, os.getcwd() + '/modules')
 from DatabaseInit import *
 
-## TODO: Change to False after I've set up the inner function logic
+def BeforeTest():
+
+    #The insanity below is because windows likes to open files again for an arbitary reason,
+    #even after you have closed them for a given duration.
+    #Please leave this alone until I can find a more elegant solution
+    # - Damien
+    while(os.path.exists(os.getcwd() + '/' + 'MTG_Database.sqlite.sha256') or os.path.exists(os.getcwd() + '/' + 'MTG_Database.sqlite')):
+        time.sleep(2)
+        try:
+            if os.path.exists(os.getcwd() + '/' + 'MTG_Database.sqlite.sha256'):
+                os.remove('MTG_Database.sqlite.sha256')
+            if os.path.exists(os.getcwd() + '/' + 'MTG_Database.sqlite'):
+                os.remove('MTG_Database.sqlite')
+        except Exeception as e:
+            print(e)
+
+BeforeTest()
+
 def test_HashFile_Does_Not_Exist():
-    assert FileExists('AllPrintings.sqlite.sha256') == True
+    assert FileExists('MTG_Database.sqlite.sha256') == False
+
+def test_Database_Does_Not_Exist():
+    assert FileExists('MTG_Database.sqlite') == False
 
 def test_HashFile_Download():
-    assert DownloadFile('https://mtgjson.com/api/v5/AllPrintings.sqlite.sha256') == True
-
-def test_HashFile_Exists():
-    assert FileExists('AllPrintings.sqlite.sha256') == True
-
-## TODO: Change to False after I've set up the inner function logic
-def test_Database_Does_Not_Exist():
-    assert FileExists('MTG_Database.sqlite') == True
+    assert DownloadFile('https://mtgjson.com/api/v5/AllPrintings.sqlite.sha256', 'MTG_Database.sqlite.sha256') == True
 
 def test_Database_Download():
-    assert DownloadFile('https://mtgjson.com/api/v5/AllPrintings.sqlite') == True
+    assert DownloadFile('https://mtgjson.com/api/v5/AllPrintings.sqlite', 'MTG_Database.sqlite') == True
+
+
+def test_HashFile_Exists():
+    assert FileExists('MTG_Database.sqlite.sha256') == True
 
 def test_Database_Exists():
     assert FileExists('MTG_Database.sqlite') == True
 
 def test_HashFile_Compared_To_Database_Hash():
-    assert HashCompare('MTG_Database.sqlite')
+    assert HashCompare('MTG_Database.sqlite') == True
 
 def test_HashFile_Compared_To_Other_File():
-    assert HashCompare('Test.txt')
+    assert HashCompare('Test.txt') == False
